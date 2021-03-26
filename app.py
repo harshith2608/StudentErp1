aadf213d2da3c8d9a12973797673bf41cd867168
Pola Harshith <pola.harshith2000@gmail.com>
2021-03-26 18:13:46 +0530

    Home, login, register connected

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/login')
+def login():
+    return render_template('Login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('Register.html')
+
+
+@app.route('/home')
+def home():
+    return render_template('Home.html')
 
 
 if __name__ == '__main__':
